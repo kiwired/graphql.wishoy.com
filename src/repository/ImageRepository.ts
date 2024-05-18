5a134e2c916aff27ab08cf923deccbf1a4fac6df
Max D <maksimdoronin93@gmail.com>
2024-05-18 13:01:41 +0200

    chore: update image repository

--- src/repository/ImageRepository.ts
+++ src/repository/ImageRepository.ts
@@ -1,5 +1,14 @@
 import { dataSource } from '../dataSource'
 import { Image } from '../entity'
 
-export const ImageRepository = dataSource.getRepository(Image).extend({})
+export const ImageRepository = dataSource.getRepository(Image).extend({
+
+	async findOneOrCreate(src: Image['src'], data: Partial<Image>) {
+		let fined = await this.findOneBy({ src })
+		if (!fined) {
+			fined = this.create({ src, ...data })
+		}
+		return fined.save({ data })
+	},
+})
 
